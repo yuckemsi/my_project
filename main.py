7e9d18f1d10999629d395493bb362fb446f4849a
yuckemsi <109517284+yuckemsi@users.noreply.github.com>
2023-10-01 21:12:18 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
-meme_dict = {
-            "КРИНЖ": "Что-то очень странное или стыдное",
-            "ЛОЛ": "Что-то очень смешное"
-            "РОФЛ": "шутка"
-            "ЩИЩ": "одобрение или восторг"
-            "КРИПОВЫЙ": "страшный, пугающий"
-            "АГРИТЬСЯ": "злиться"
-            }
-word = input("Введите непонятное слово (большими буквами!): ")
+from settings import *
+import discord
+from discord.ext import commands
 
-if word in meme_dict.keys():
-    print(meme_dict[word])
-else:
-    print("Кажется вашего слова нет в нашем словаре :)")
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Привет! Я бот {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def bye(ctx):
+    await ctx.send("Пока, до следующего использования!")
+
+bot.run(settings["TOKEN"])
