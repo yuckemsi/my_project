735008fda66179894e35ce13ee0e02a2018d9c51
yuckemsi <109517284+yuckemsi@users.noreply.github.com>
2023-10-22 19:23:49 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from settings import *
 import discord
 from discord.ext import commands
+import os,random
+from settings import *
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='$', intents=intents)
+bot = commands.Bot(command_prefix="$", intents=intents)
 
 @bot.event
 async def on_ready():
@@ -22,5 +23,27 @@ async def heh(ctx, count_heh = 5):
 @bot.command()
 async def bye(ctx):
     await ctx.send("Пока, до следующего использования!")
+
+@bot.command()
+async def mem(ctx):
+    img_name = random.choice(os.listdir('images'))
+    with open(f'images/{img_name}', 'rb') as f:
+        # В переменную кладем файл, который преобразуется в файл библиотеки Discord!
+        picture = discord.File(f)
+   # Можем передавать файл как параметр!
+    await ctx.send(file=picture)
+
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+
+@bot.command('duck')
+async def duck(ctx):
+    '''По команде duck вызывает функцию get_duck_image_url'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
 
 bot.run(settings["TOKEN"])
